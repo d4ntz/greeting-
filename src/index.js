fd8b82ebd446199ba7ff99f9f7f9061fb6f9a5f4
d4ntz <purbashis31@gmail.com>
2021-10-24 10:22:23 +0530

    greeting app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css' ;
+let curDate = new Date();
+curDate = curDate.getHours();
+let greeting =' ';
+
+const cssStyle={
+
+
+};
+const curdate=new Date().toLocaleDateString();
+const curtime=new Date().toLocaleTimeString();
+
+
+
+if (curDate >=1 &&  curDate < 12)
+{
+  greeting  ="Good Morning  ";
+
+  cssStyle.color ='lime';
+
+  
+}else if (curDate>=12 && curDate <19)
+{
+  greeting  ="Good Afternoon"; 
+  cssStyle.color ='lightblue';
+}
+
+else
+{
+  greeting  = "  Good Night" ;
+  cssStyle.color ='dark';
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+<div>
+<h2 className="time">
+  Time:  {curtime}
+</h2>
+</div>
+<div>
+<h1 style ={ { fontFamily: 'Brush Script MT'
+
+}}>
+Hello Sir, <span style={cssStyle}>{  greeting }</span>
+
+
+</h1>
+</div>
+<div>
+<h2 className="date">
+Date : {curdate}
+</h2>
+
+</div>
+
+
+</>
+ 
+ 
+ ,
+
+  document.getElementById('root'));
